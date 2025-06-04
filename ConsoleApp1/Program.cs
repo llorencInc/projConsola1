bba8c4fa913afb4be8b666cb766bee8941066669
CarlesFDJ <a24carfradej@inspedralbes.cat>
2025-06-04 15:34:11 +0200

    prove

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Using llibreriesProva;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public void requadre()
+{
+    for (int i = 1; i =< 5; i++)
+    Console.WriteLine("*");
+}
+
+//requadre();
