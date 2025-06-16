9146940cb2050617ad0f033ffda2eb1dc95b5ef0
CarlesFDJ <a24carfradej@inspedralbes.cat>
2025-06-16 12:20:35 +0200

    c

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba8c4fa913afb4be8b666cb766bee8941066669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f64d3dbdd481b9205f359b01cad3c9b9ea59212")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
